f4dde760e8d991f8233361f410c1e5e582b49c27
Han Jiang <jianghan@vivo.com>
2025-10-16 09:46:45 +0800

    Add cmsis timer support (#84)

--- kernel/src/boards/raspberry_pico2_cortexm/mod.rs
+++ kernel/src/boards/raspberry_pico2_cortexm/mod.rs
@@ -209,7 +209,7 @@ pub(crate) fn get_cycles_to_duration(cycles: u64) -> core::time::Duration {
     );
 }
 
-pub(crate) fn get_cycles_to_ms(cycles: u64) -> u64 {
+pub fn get_cycles_to_ms(cycles: u64) -> u64 {
     return (cycles as u128 * 1_000 as u128 / config::PLL_SYS_FREQ as u128) as u64;
 }
 
