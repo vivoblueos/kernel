2b73f8a1cb9d90d56dfc7622b33db1936014f041
Kai Law <lawkai@vivo.com>
2025-11-19 17:42:00 +0800

    Support nested instrusive adapters (#151)

--- kernel/src/types.rs
+++ kernel/src/types.rs
@@ -15,7 +15,9 @@
 use crate::sync::{ISpinLock, SpinLock, SpinLockGuard};
 pub use blueos_infra::{
     impl_simple_intrusive_adapter,
-    intrusive::{Adapter as IntrusiveAdapter, Relative as RelativeAdapter},
+    intrusive::{
+        Adapter as IntrusiveAdapter, Nested as NestedAdapter, Relative as RelativeAdapter,
+    },
     list::{
         typed_atomic_ilist::AtomicListHead as AtomicIlistHead, typed_ilist::ListHead as IlistHead,
         GenericList,
