c83ba54353a1ff8d8cf5f9d070cf744fd09810de
Kai Law <lawkai@vivo.com>
2025-11-21 17:01:11 +0800

    Go

--- adapter/thread_metric/src/lib.rs
+++ adapter/thread_metric/src/lib.rs
@@ -13,3 +13,73 @@
 // limitations under the License.
 
 #![no_std]
+
+use blueos::{
+    scheduler, thread,
+    thread::{READY, SUSPENDED},
+};
+use blueos_kconfig::TICKS_PER_SECOND;
+use core::mem::MaybeUninit;
+use libc::c_int;
+
+extern "C" {
+    fn tm_main();
+}
+
+#[used]
+#[link_section = ".bk_app_array"]
+static TM_MAIN: unsafe extern "C" fn() = tm_main;
+const TM_SUCCESS: c_int = 0;
+const TM_ERROR: c_int = 1;
+
+const MAX_THREADS: usize = 16;
+static mut TM_THREADS: [MaybeUninit<ThreadNode>; MAX_THREADS] =
+    [MaybeUninit::zeroed(); MAX_THREADS];
+
+#[no_mangle]
+pub extern "C" fn tm_initialize(test_initialization_function: extern "C" fn()) {
+    test_initialization_function()
+}
+
+#[no_mangle]
+pub extern "C" fn tm_thread_create(
+    thread_id: c_int,
+    priority: c_int,
+    entry: extern "C" fn(),
+) -> c_int {
+    let builder = thread::Builder::new(Entry::C(entry));
+    let t = builder.set_priority(priority).build();
+    unsafe {
+        TM_THREADS[thread_id as usize].write(t);
+    }
+    TM_SUCCESS
+}
+
+#[no_mangle]
+pub extern "C" fn tm_thread_resume(thread_id: c_int) -> c_int {
+    let t = unsafe { TM_THREADS[thread_id as usize].asseum_init_ref().clone() };
+    if scheduler::queue_ready_thread(t.state(), t) {
+        return TM_SUCCESS;
+    }
+    TM_ERROR
+}
+
+#[no_mangle]
+pub extern "C" fn tm_thread_suspend(thread_id: c_int) -> c_int {
+    let t = unsafe { TM_THREADS[thread_id as usize].asseum_init_ref() };
+    if scheduler::remove_from_ready_queue(t) {
+        t.transfer_state(READY, SUSPENDED);
+        return TM_SUCCESS;
+    }
+    TM_ERROR
+}
+
+#[no_mangle]
+pub extern "C" fn tm_thread_relinquish() {
+    scheduler::relinquish_me()
+}
+
+#[no_mangle]
+pub extern "C" fn tm_thread_sleep(secs: c_int) {
+    scheduler::suspend_me_for(TICKS_PER_SECOND * secs as usize);
+}
