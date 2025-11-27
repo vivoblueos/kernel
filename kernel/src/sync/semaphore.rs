9331af9a243393662604efd7ccf5150a288f9fcb
jinwjinl <2532191107@qq.com>
2025-11-27 17:32:58 +0800

    replace all assert under kernel with debug_assert

--- kernel/src/sync/semaphore.rs
+++ kernel/src/sync/semaphore.rs
@@ -78,8 +78,17 @@ impl Semaphore {
     where
         M: InsertModeTrait,
     {
+<<<<<<< HEAD
+        debug_assert!(!irq::is_in_irq());
+=======
+<<<<<<< HEAD
+        assert!(!irq::is_in_irq());
+>>>>>>> 1d4c732 (replace all assert under kernel with debug_assert)
+        let this_thread = scheduler::current_thread();
+=======
         debug_assert!(!irq::is_in_irq());
         let this_thread = scheduler::current_thread();
+>>>>>>> 919b619 (replace all assert under kernel with debug_assert)
         let mut w = self.pending.irqsave_lock();
         loop {
             let old = self.counter.get();
@@ -111,6 +120,7 @@ impl Semaphore {
         M: InsertModeTrait,
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
         assert!(!irq::is_in_irq());
         let this_thread = scheduler::current_thread();
 =======
@@ -120,6 +130,18 @@ impl Semaphore {
 =======
         let this_thread = scheduler::current_thread();
 >>>>>>> 95f7a35 (Resolved merge conflicts)
+=======
+        debug_assert!(!irq::is_in_irq());
+=======
+<<<<<<< HEAD
+        assert!(!irq::is_in_irq());
+>>>>>>> 1d4c732 (replace all assert under kernel with debug_assert)
+        let this_thread = scheduler::current_thread();
+=======
+        debug_assert!(!irq::is_in_irq());
+        let this_thread = scheduler::current_thread();
+>>>>>>> 919b619 (replace all assert under kernel with debug_assert)
+>>>>>>> 40ff17d (replace all assert under kernel with debug_assert)
         let mut w = self.pending.irqsave_lock();
         let mut last_sys_ticks = crate::time::get_sys_ticks();
         loop {
