8fdac14ca1abb59268c63e19bf45a360e99ea759
xuchang-vivo <xuchang0929@vivo.com>
2026-02-02 17:14:30 +0800

    minor

--- driver/src/systimer/mod.rs
+++ driver/src/systimer/mod.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 vivo Mobile Communication Co., Ltd.
+// Copyright (c) 2026 vivo Mobile Communication Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,14 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![no_std]
-#![feature(const_nonnull_new)]
-
-pub mod clock_control;
-pub mod i2c;
-pub mod pinctrl;
-pub mod reset;
-pub mod static_ref;
-pub mod systimer;
-pub mod uart;
