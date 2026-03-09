447dfe242b7911074a5e0b5b76c7bd8feca37e3d
xuchang-vivo <xuchang0929@vivo.com>
2026-03-09 11:57:24 +0800

    minor

--- driver/src/systimer/esp32_sys_timer.rs
+++ driver/src/systimer/esp32_sys_timer.rs
@@ -222,7 +222,7 @@ impl<const BASE_ADDR: usize, const HZ: u64> Clock for Esp32SysTimer<BASE_ADDR, H
         // See https://github.com/espressif/qemu/issues/69
         let now = Self::estimate_current_cycles();
         let compensation = core::cmp::max(1, HZ / 100_00); // ~100us
-        let moment = if moment < now {
+        let moment = if moment < now + compensation {
             now.saturating_add(compensation)
         } else {
             moment
