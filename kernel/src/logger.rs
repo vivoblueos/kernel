986efecda7986b758eb216ab105997d6bbb787b5
xuchang-vivo <xuchang0929@vivo.com>
2026-04-27 14:10:34 +0800

    change println port in test mode

--- kernel/src/logger.rs
+++ kernel/src/logger.rs
@@ -61,8 +61,17 @@ impl log::Log for Logger {
         let timestamp = time::now().as_millis();
         let tid = scheduler::current_thread_id();
         let cpu = arch::current_cpu_id();
-        let _guard = LOGGER_MUTEX.irqsave_lock();
+        #[cfg(not(test))]
         kprintln!(
+            "[T:{:09} C:{} TH:0x{:x}][{}] {} ",
+            timestamp,
+            cpu,
+            tid,
+            record.level(),
+            record.args()
+        );
+        #[cfg(test)]
+        semihosting::println!(
             "[T:{:09} C:{} TH:0x{:x}][{}] {} ",
             timestamp,
             cpu,
